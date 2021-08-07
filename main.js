a673b22317ee2a6ac21e6c9584a148eb2e10df9f
rose-sun <15051858768@163.com>
2021-08-07 14:41:36 +0800

    虚拟键盘

---
+++ main.js
@@ -0,0 +1,50 @@
+const $ = s => document.querySelector(s)
+const $$ = s => document.querySelectorAll(s)
+
+let text = ''
+
+$('.input').onclick = function(e) {
+  this.classList.add('focus')
+  $('.keyboard').classList.add('show')
+  e.stopPropagation()
+}
+
+document.onclick = function() {
+  $('.input').classList.remove('focus')
+  $('.keyboard').classList.remove('show')
+}
+
+$('.keyboard').onclick = function(e) {
+  e.stopPropagation() 
+}
+
+$$('.keyboard .row > span').forEach($key => {
+  $key.onclick = function(e) {
+    let type = this.dataset.type
+    console.log(type)
+    switch(type) {
+      case 'char':
+        text += this.innerText
+        $('.input').innerText = text
+        break;
+      case 'uppercase':
+        setPage('uppercase')
+        break;
+      case 'lowercase':
+        setPage('lowercase')
+        break;
+      case 'number':
+        setPage('number')
+        break;
+      case 'symbol':
+        setPage('symbol');
+        break;
+    }
+  }
+})
+
+function setPage(name) {
+  $$('.keyboard .page').forEach($page => $page.style.display = 'none')
+  $('.keyboard .page-' + name).style.display = 'block'
+}
+
